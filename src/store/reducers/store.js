e12712671433dedb1dd8ba6b2bdca2c0108336ef
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-20 02:55:12 +0700

    can Fetch data from backend

--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { productReducer } from "./ProductReducers";
 
 export const store = configureStore({
-    reducer: {},
-    preloadedState: {},
+    reducer: {
+         products: productReducer
+    },
+    preloadedState: {
+       
+    },
 });
 
 export default store;
