64db87f4f9861f5fd3171205021311ca704e7eff
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-20 00:45:34 +0700

    UI on ProductCard

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,14 +3,14 @@ import ProductCard from "./ProductCard";
 
 const Products = () =>{
     const isLoading = false;
-    const errorMessage = "Error Fecthing Products";
+    const errorMessage = "";
    const products = [
   {
     productId: 652,
     productName: "Iphone Xs max",
     image: "https://placehold.co/600x400",
     description: "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
-    quantity: 0,
+    quantity: 15,
     price: 1450.0,
     discount: 10.0,
     specialPrice: 1305.0,
