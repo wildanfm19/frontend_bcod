b1e8bb727554ab82373cde6932544747e238ecd1
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-20 00:04:38 +0700

    Product , ProductCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { FaBeer } from 'react-icons/fa'
+import Products from './components/Product'
 
 function App() {
   return(
-    <div className='flex items-center justify-center h-screen bg-gray-800 text-white'>
-      <FaBeer/>
-    </div>
+    
+      <Products/>
+    
   )
 }
 
-export default App
+export default App;
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,11 @@
+const ProductCard = () => {
+    
+
+    return(
+        <div className="">
+            <h1>Product</h1>
+        </div>
+    )
+}
+
+export default ProductCard;
