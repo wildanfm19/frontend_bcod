7b7745cc083bf55bdc43da917ed35ea0cbfb64bd
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-19 23:42:10 +0700

    Dependency

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { FaBeer } from 'react-icons/fa'
+
+function App() {
+  return(
+    <div className='flex items-center justify-center h-screen bg-gray-800 text-white'>
+      <FaBeer/>
+    </div>
+  )
+}
+
+export default App
