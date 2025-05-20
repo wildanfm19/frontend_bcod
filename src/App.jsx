7fbc187e8aa509973a9271e88e9ef4c413ce326f
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-20 19:59:03 +0700

    Routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-
 import './App.css'
+import Home from './components/home/Home';
 import Products from './components/products/Product'
-
+import { BrowserRouter as Router , Routes ,Route } from 'react-router-dom';
 function App() {
   return(
-    
-      <Products/>
-    
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+         <Route path='/products' element={<Products/>}/>
+      </Routes>
+    </Router>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -11,9 +11,7 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
  <Provider store={store}>
   <StrictMode>
-    <BrowserRouter>
       <App />
-      </BrowserRouter>
     </StrictMode>
   </Provider>
   
