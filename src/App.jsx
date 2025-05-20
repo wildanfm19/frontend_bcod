5bc6ec3d4cf76e70c85bf7eb296995c640e53df0
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-21 01:08:49 +0700

    Start on Shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Contact from './components/Contact';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Toaster } from 'react-hot-toast'; // Needed to show toast notifications
+import Cart from './components/cart/Cart';
 
 function App() {
   return(
@@ -21,6 +22,7 @@ function App() {
           <Route path='/products' element={<Products/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/contact' element={<Contact/>}/>
+          <Route path='/cart' element={<Cart/>}/>
         </Routes>
     </Router>
     <Toaster position= 'bottom-center'/>
