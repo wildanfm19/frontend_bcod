d9a2e4f9681bfac7532fc68460fe03a76c7428d4
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-21 00:43:54 +0700

    Makes Notifcation when add item to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import React from 'react'; // Needed for React.Fragment
+import './App.css';
+
 import Home from './components/home/Home';
-import Products from './components/products/Product'
-import { BrowserRouter as Router , Routes ,Route } from 'react-router-dom';
+import Products from './components/products/Product';
 import Navbar from './components/shared/Navbar';
 import About from './components/About';
 import Contact from './components/Contact';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Toaster } from 'react-hot-toast'; // Needed to show toast notifications
 
 function App() {
   return(
+    <React.Fragment>
     <Router>
       <Navbar/>
         <Routes>
@@ -18,6 +23,8 @@ function App() {
           <Route path='/contact' element={<Contact/>}/>
         </Routes>
     </Router>
+    <Toaster position= 'bottom-center'/>
+    </React.Fragment>
   )
 }
 
--- src/components/shared/ProductCard.jsx
+++ src/components/shared/ProductCard.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import ProductViewModal from "./ProductViewModal";
 import truncateText from "../../utils/truncateText";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../store/actions";
+import toast from "react-hot-toast";
 
 const ProductCard = ({
     productId,
@@ -18,6 +21,7 @@ const ProductCard = ({
     const btnLoader = false;
     const [selectedViewProduct , setSelectedViewProduct] =useState("");
     const isAvailable = quantity && Number(quantity) > 0;
+    const dispatch = useDispatch();
 
     const handleProductView = (product) => {
         if(!about){
@@ -25,6 +29,10 @@ const ProductCard = ({
             setOpenProductViewModal(true);
         }
         
+    };
+
+    const addToCartHandler = (cartItems) => {
+        dispatch(addToCart(cartItems,1,toast));
     }
 
     return(
@@ -80,7 +88,15 @@ const ProductCard = ({
 
                 <button
                     disabled= {!isAvailable || btnLoader}
-                    onClick={() => {}}
+                    onClick={() => addToCartHandler({
+                        image,
+                        productName,
+                        description,
+                        specialPrice,
+                        price,
+                        productId,
+                        quantity
+                    })}
                     className={`bg-blue-500 ${isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"}
                         text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}>
                         <FaShoppingCart/>
