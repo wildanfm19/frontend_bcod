b4e9b8bad364676e999178f71bbdf9488896f2d0
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-21 02:46:31 +0700

    Remove Product From Cart

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -109,3 +109,9 @@ export const decreaseCartQuantity =
         });
         localStorage.setItem("cartItems", JSON.stringify(getState().carts.cart));
     }
+
+    export const removeFromCart =  (data, toast) => (dispatch, getState) => {
+    dispatch({type: "REMOVE_CART", payload: data });
+    toast.success(`${data.productName} removed from cart`);
+    localStorage.setItem("cartItems", JSON.stringify(getState().carts.cart));
+}
--- src/store/reducers/cartReducers.js
+++ src/store/reducers/cartReducers.js
@@ -33,6 +33,15 @@ export const cartReducer = (state = initialState , action) => {
                     cart:newCart,
                 };
             }
+
+             case "REMOVE_CART":
+            return {
+                ...state,
+                cart: state.cart.filter(
+                    (item) => item.productId !== action.payload.productId
+                ),
+            };
+            
         default:
             return state;
     }
