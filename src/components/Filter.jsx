556995e7389f995b2aa33bcf7feb81b09d87140d
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-20 17:58:49 +0700

    SortButton working , ClearButton working

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,73 +1,153 @@
-import { MenuItem , Select , FormControl, InputLabel, Tooltip, Button } from "@mui/material";
-import { useState } from "react";
-import { FiArrowUp, FiRefreshCcw, FiSearch } from "react-icons/fi";
+import {
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  Tooltip,
+  Button
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import {
+  FiArrowDown,
+  FiArrowUp,
+  FiRefreshCcw,
+  FiSearch
+} from "react-icons/fi";
+import {
+  useLocation,
+  useNavigate,
+  useSearchParams
+} from "react-router-dom";
 
-const Filter  = () => {
-    const categories = [
-        {categoryId: 1 , categoryName: "Electronics"},
-        {categoryId: 1 , categoryName: "Clothing"},
-        {categoryId: 1 , categoryName: "Books"},
-        {categoryId: 1 , categoryName: "Toys"},
+const Filter = () => {
+  const categories = [
+    { categoryId: 1, categoryName: "Electronics" },
+    { categoryId: 2, categoryName: "Clothing" },
+    { categoryId: 3, categoryName: "Books" },
+    { categoryId: 4, categoryName: "Toys" }
+  ];
 
-    ];
+  const [searchParams] = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = useLocation().pathname;
+  const navigate = useNavigate();
 
-    const [category , setCategory] = useState("all");
+  const [category, setCategory] = useState("all");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleCategoryChange = (event) =>{
-        setCategory(event.target.value);
-    };
+  useEffect(() => {
+    const currentCategory = searchParams.get("category") || "all";
+    const currentSortOrder = searchParams.get("sortby") || "asc";
+    const currentSearchTerm = searchParams.get("keyword") || "";
 
-    return (
-        <div className="flex lg:flex-row flex-col-reverse lg:justify-between justify-center items-center gap-4">
-            {/* SEARCH BAR */}
-            <div className="relative flex items-center 2xl:w-[450px] sm:w-[420px] w-full">
-                <input 
-                type="text"
-                placeholder="Search Products"
-                className="border border-gray-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:outline-none focus:ring-2 focus:ring-[#1976d2]"
-                >
-                </input>
-                <FiSearch className ="absolute left-3 text-slate-800" size={20}/>
-            </div>
+    setCategory(currentCategory);
+    setSortOrder(currentSortOrder);
+    setSearchTerm(currentSearchTerm);
+  }, [searchParams]);
 
-            <div className="flex sm:flex-row flex-col gap-4 items-center ">
-                <FormControl 
-                    className="text-slate-800 border-slate-700"
-                    variant="outlined"
-                    size="small">
-                        <InputLabel Id="category-select-label">Category</InputLabel>
-                        <Select
-                            labelId="category-select-label"
-                            value={category}
-                            onChange={handleCategoryChange}
-                            label= "Category"
-                            className="min-w-[120px] text-slate-800 border-slate-700"
-                            >
-                            
-                        {/* CATEGORY SELECTION */}
-                            <MenuItem value="all">All</MenuItem>
-                            {categories.map((item) => (
-                                <MenuItem key={item.categoryId} value={item.categoryName}>
-                                    {item.categoryName}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                </FormControl>
+  useEffect(() => {
+    const handler = setTimeout(() => {
+        if (searchTerm) {
+            searchParams.set("keyword",searchTerm);
+        } else{
+            searchParams.delete("keyword");
+        }
+        navigate(`${pathname}?${searchParams.toString()}`);
+    } , 700);
 
-                {/* SORT BUTTON */}
-                <Tooltip title="Sorted by Price: asc">
-                    <Button variant="contained" color="primary" className="flex items-center gap-2 h-10">
-                        Sort By
-                        <FiArrowUp size={20}/>
-                    </Button>
-                </Tooltip>
-                <button className="flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md transition duration-300 ease-in shadow-md focus:outline-none">
-                        <FiRefreshCcw className="font-semibold" size={16}/>
-                        <span className="font-semibold">Clear Filter</span>
-                </button>
-            </div>
-        </div>
-    )
-}
+    return () => {
+        clearTimeout(handler);
+    }
+  },[searchParams,searchTerm , navigate , pathname])
+
+  const handleCategoryChange = (event) => {
+    const selectedCategory = event.target.value;
+
+    if (selectedCategory === "all") {
+      params.delete("category");
+    } else {
+      params.set("category", selectedCategory);
+    }
+
+    navigate(`${pathname}?${params.toString()}`);
+    setCategory(selectedCategory);
+  };
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevOrder) => {
+      const newOrder = prevOrder === "asc" ? "desc" : "asc";
+      params.set("sortby", newOrder);
+      navigate(`${pathname}?${params.toString()}`);
+      return newOrder;
+    });
+  };
+
+  const handleClearFilter = () => {
+   navigate({pathname : window.location.pathname})
+  };
+
+  return (
+    <div className="flex lg:flex-row flex-col-reverse lg:justify-between justify-center items-center gap-4">
+      {/* SEARCH BAR */}
+      <div className="relative flex items-center 2xl:w-[450px] sm:w-[420px] w-full">
+        <input
+          type="text"
+          placeholder="Search Products"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border border-gray-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:outline-none focus:ring-2 focus:ring-[#1976d2]"
+        />
+        <FiSearch className="absolute left-3 text-slate-800" size={20} />
+      </div>
+
+      <div className="flex sm:flex-row flex-col gap-4 items-center">
+        {/* CATEGORY DROPDOWN */}
+        <FormControl variant="outlined" size="small">
+          <InputLabel id="category-select-label">Category</InputLabel>
+          <Select
+            labelId="category-select-label"
+            value={category}
+            onChange={handleCategoryChange}
+            label="Category"
+            className="min-w-[120px] text-slate-800"
+          >
+            <MenuItem value="all">All</MenuItem>
+            {categories.map((item) => (
+              <MenuItem key={item.categoryId} value={item.categoryName}>
+                {item.categoryName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        {/* SORT BUTTON */}
+        <Tooltip title={`Sorted by Price: ${sortOrder}`}>
+          <Button
+            variant="contained"
+            onClick={toggleSortOrder}
+            color="primary"
+            className="flex items-center gap-2 h-10"
+          >
+            Sort By
+            {sortOrder === "asc" ? (
+              <FiArrowUp size={20} />
+            ) : (
+              <FiArrowDown size={20} />
+            )}
+          </Button>
+        </Tooltip>
+
+        {/* CLEAR BUTTON */}
+        <button className="flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md transition duration-300 ease-in shadow-md focus:outline-none"
+        onClick={handleClearFilter}
+        >
+          <FiRefreshCcw className="font-semibold" size={16} />
+          <span className="font-semibold">Clear Filter</span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Filter;
