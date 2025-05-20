3a37499e495af050b3c5dab6c706cc5e4d552ab1
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-20 22:33:23 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Home from './components/home/Home';
 import Products from './components/products/Product'
 import { BrowserRouter as Router , Routes ,Route } from 'react-router-dom';
+import Navbar from './components/shared/Navbar';
 function App() {
   return(
     <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-         <Route path='/products' element={<Products/>}/>
-      </Routes>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/products' element={<Products/>}/>
+        </Routes>
     </Router>
   )
 }
