30ad5c7eb4a7368084503b119062c9ecd2c620b9
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-21 22:11:06 +0700

    gambar

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,8 @@
 import bannerImageOne from "../assets/sliders/IPHONE-Photoroom.png"
-
+import bannerImageTwo from "../assets/sliders/sepatu_nike.png"
+import bannerImageThree from   "../assets/sliders/komik.jpg"
 export{
-    bannerImageOne
+    bannerImageOne,
+    bannerImageTwo,
+    bannerImageThree
 }
